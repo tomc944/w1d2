b7f804fe3be776557544bb86dd7a02bb5a3c8a1c
Thomas Claiborne <thomas.claiborne3@gmail.com>
2015-10-27 21:04:02 -0700

    minor refactoring

--- card.rb
+++ card.rb
@@ -1,5 +1,5 @@
 class Card
-  attr_accessor :face_value, :face_down
+  attr_reader :face_value, :face_down
 
   def initialize(face_value, face_down = true)
     @face_value = face_value.to_s
@@ -14,22 +14,11 @@ class Card
     @face_down = false
   end
 
-  def readInfo
-    return @face_value unless @face_down
-    nil
-    #
-    # if @face_down
-    #   " "
-    # else
-    #   @face_value
-    # end
-    #
-    # @face_down ? " " : @face_value
+  def to_s
+    @face_down ? "-" : "#{@face_value}"
   end
 
   def ==(previousCard)
-    # return @face_value == previousCard.face_value
-    # false
-    @face_value == previousCard.face_value
+    face_value == previousCard.face_value && previousCard.is_a?(self.class)
   end
 end
